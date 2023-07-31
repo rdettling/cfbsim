1a723c8c1a7ab7aa3d8a67b84fcb7d705ca59411
rdettling <ryandettling1@gmail.com>
2023-07-30 18:40:32 -0700

    postgres

--- cfbsim/settings.py
+++ cfbsim/settings.py
@@ -85,12 +85,12 @@ WSGI_APPLICATION = 'cfbsim.wsgi.application'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 # Use PostgreSQL in production
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
 DATABASES = {
     'default': {
