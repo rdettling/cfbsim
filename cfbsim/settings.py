dc30513c552701ebdba66076ff00eaa03fdbe2cf
rdettling <ryandettling1@gmail.com>
2023-12-01 13:34:49 -0500

    collect static

--- cfbsim/settings.py
+++ cfbsim/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     "django.contrib.auth",
     "django.contrib.contenttypes",
     "django.contrib.sessions",
+    "django.contrib.staticfiles",
     "django.contrib.messages",
     "start",
     "team",
