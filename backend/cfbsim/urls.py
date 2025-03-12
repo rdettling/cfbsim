620406863b26f771819741f79db85a00a92ec5c5
rdettling <ryandettling1@gmail.com>
2025-03-12 01:20:02 -0700

    health check

--- backend/cfbsim/urls.py
+++ backend/cfbsim/urls.py
@@ -2,10 +2,15 @@ from django.contrib import admin
 from django.urls import path, include, re_path
 from django.views.generic import TemplateView
 from cfbsim.settings import DEV
+from django.http import HttpResponse
+
+def health_check(request):
+    return HttpResponse("Server is running")
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("start.urls")),
+    path('health/', health_check),
     # path("recruit/", include("recruit.urls")),
 ]
 
