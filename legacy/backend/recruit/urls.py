639ec65fbfbf09ee68b96be04f16c2ccbfb47a58
rdettling <ryandettling1@gmail.com>
2026-02-15 17:15:29 -0800

    remove legacy

--- legacy/backend/recruit/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path("", views.recruiting),
-    path("offer/<int:id>", views.offer),
-    path("add_points/<int:id>", views.points),
-]
